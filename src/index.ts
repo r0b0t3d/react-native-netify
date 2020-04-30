aa22b00b954006fd3cb763a91ef742b0868d34a5
Tuan Luong <tuanluong.it@gmail.com>
2020-04-30 10:15:08 +0700

    handle error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules } from 'react-native';
 
 const { Netify } = NativeModules;
-const isIos = Platform.OS === 'ios';
 
 const defaultConfig: Config = {
   timeout: 60,
@@ -22,31 +21,13 @@ async function jsonRequest(params: JsonRequestParams) {
     const response = await Netify.jsonRequest({ ...params, url });
     return response;
   } catch (error) {
-    if (isIos) {
-      handleIosError(error);
+    const { userInfo } = error;
+    if (userInfo) {
+      const { response } = userInfo;
+      error.response = response;
+      throw error;
     }
-    handleAndroidError(error);
-  }
-}
-
-function handleAndroidError(error: any) {
-  console.log('Error', JSON.stringify(error));
-  const { code, userInfo } = error;
-  if (userInfo) {
-    const { response } = userInfo;
-    error.response = response;
     throw error;
-  }
-  throw error;
-}
-
-function handleIosError(error: any) {
-  if (error.userInfo) {
-    const { response } = error.userInfo;
-    const err = new Error(error.message);
-    // @ts-ignore
-    err.response = response;
-    throw err;
   }
 }
 
